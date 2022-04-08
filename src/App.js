691e40954a55ea39f33e17ffb400a451521392f8
Cindi-Amalia <cndamalia01@gmail.com>
2022-04-08 08:09:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import CreatePlaylist from './pages/CreatePlaylist';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import GuardRoute from './components/GuardRoute';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <GuardRoute path="/create-playlist" type="private" exact>
+          <CreatePlaylist/>
+        </GuardRoute>
+        <GuardRoute path="/" type="guest" exact>
+          <Auth/>
+        </GuardRoute>
+        <Route path="*">
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+export default App;
